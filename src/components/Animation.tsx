639bd9092271c5297e7d1765ede919a439a1fe43
Floria Gu <gfzting@gmail.com>
2022-05-08 23:46:34 -0700

    Skip animations when prefers-reduced-motion detected

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,5 +1,6 @@
-import React, { PropsWithChildren, useState } from "react";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import { InView } from "react-intersection-observer";
+import { Globals } from "react-spring";
 
 export function AnimateWhenReached({children, ...props}: PropsWithChildren<any>) {
     const [reached, setReached] = useState(false);
@@ -30,4 +31,25 @@ export function AnimateWhenHovered({children, ...props}: PropsWithChildren<any>)
             {children(hover)}
         </div>
     )
+}
+
+export function SetAnimationPreference() {
+    const [prefersReducedMotion, setPrefersReducedMotion] = useState(() => window.matchMedia("(prefers-reduced-motion)").matches);
+
+    useEffect(() => {
+        const handlePreferenceChange = (event: MediaQueryListEvent) => setPrefersReducedMotion(event.matches);
+
+        const mediaQuery = window.matchMedia("(prefers-reduced-motion)");
+
+        mediaQuery.addEventListener("change", handlePreferenceChange);
+        return () => mediaQuery.removeEventListener("change", handlePreferenceChange);
+    }, []);
+
+    useEffect(() => {
+        Globals.assign({
+            skipAnimation: prefersReducedMotion,
+        });
+    }, [prefersReducedMotion]);
+
+    return null;
 }
