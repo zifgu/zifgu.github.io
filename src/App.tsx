155925e02d2a77496bcc59bfb352b4570fad4e9f
Floria Gu <gfzting@gmail.com>
2022-05-09 12:45:53 -0700

    Scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
 import { ProjectPage } from "./pages/ProjectPage";
 import { MainPage } from "./pages/MainPage";
 import { Layout } from "./pages/Layout";
 
 function App() {
-    // TODO: scroll restoration
     return (
         <HashRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<MainPage/>}/>
-                    <Route path="projects/:projectId" element={<ProjectPage/>}/>
-                </Route>
-            </Routes>
+            <ScrollToTop>
+                <Routes>
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<MainPage/>}/>
+                        <Route path="projects/:projectId" element={<ProjectPage/>}/>
+                    </Route>
+                </Routes>
+            </ScrollToTop>
         </HashRouter>
     );
 }
 
 export default App;
+
+function ScrollToTop(props: PropsWithChildren<any>) {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    return props.children;
+}
